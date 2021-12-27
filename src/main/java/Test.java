ac334f033ed14c648b7c1c36605591036620a725
vzikratyi <vzikratyi@playtika.com>
2021-12-27 22:23:42 +0200

    Day 10

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,206 +1,106 @@
-import lombok.Value;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
+import java.util.Stack;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        File taskFile = new File("/Users/vzikratyi/Test/9.txt");
+        File taskFile = new File("/Users/vzikratyi/Test/10.txt");
         try (Reader r = new FileReader(taskFile);
              BufferedReader br = new BufferedReader(r)) {
 
-            List<int[]> boardLines = br.lines()
-                    .map(str -> str.split(""))
-                    .map(numberStrs -> Arrays.stream(numberStrs).mapToInt(Integer::parseInt).toArray())
-                    .collect(Collectors.toList());
+            List<String> lines = br.lines().collect(Collectors.toList());
 
-            int[][] board = null;
-            for (int i = 0; i < boardLines.size(); i++) {
-                int[] boardLine = boardLines.get(i);
-                if (board == null) {
-                    board = new int[boardLines.size()][boardLine.length];
-                }
-                board[i] = boardLine;
-            }
+            BracesManager bracesManager = new BracesManager(lines);
 
-            BasinManager basinManager = new BasinManager(board);
-
-            System.out.println(basinManager.getTopLargestBasins(3));
+            System.out.println(bracesManager.getCompletionScore());
         }
     }
 
-    private static class BoardRiskCalculator {
-        List<Point> lowestPoints = new ArrayList<>();
-        private final int[][] board;
+    private static class BracesManager {
+        private static final Map<String, Integer> ERROR_SCORES = Map.of(
+                ")", 3,
+                "]", 57,
+                "}", 1197,
+                ">", 25137
+        );
+        private static final Map<String, Integer> COMPLETION_SCORES = Map.of(
+                ")", 1,
+                "]", 2,
+                "}", 3,
+                ">", 4
+        );
 
-        public BoardRiskCalculator(int[][] board) {
-            this.board = board;
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
-                    if (isLowPoint(i, j, board)) {
-                        lowestPoints.add(new Point(i, j));
+        private static final Set<String> OPENING_BRACES = Set.of(
+                "(", "[", "{", "<"
+        );
+
+        private static final Map<String, String> MATCHING_BRACES = Map.of(
+                "(", ")",
+                "[", "]",
+                "{", "}",
+                "<", ">"
+        );
+
+
+        private final List<String> foundErrorBraces = new ArrayList<>();
+        private final List<List<String>> completedLines = new ArrayList<>();
+
+        public BracesManager(Collection<String> lines) {
+            lines.stream()
+                    .filter(Predicate.not(String::isBlank))
+                    .forEach(this::validateBraces);
+        }
+
+        private void validateBraces(String line) {
+            Stack<String> openingBraces = new Stack<>();
+            for (String brace : line.split("")) {
+                if (OPENING_BRACES.contains(brace)) {
+                    openingBraces.push(brace);
+                } else {
+                    if (openingBraces.isEmpty() || !MATCHING_BRACES.get(openingBraces.pop()).equals(brace)) {
+                        foundErrorBraces.add(brace);
+                        return;
                     }
                 }
             }
-        }
-
-        private boolean isLowPoint(int i, int j, int[][] board) {
-            int pointHeight = board[i][j];
-            return pointHeight < getPointHeight(i - 1, j, board)
-                    && pointHeight < getPointHeight(i + 1, j, board)
-                    && pointHeight < getPointHeight(i, j - 1, board)
-                    && pointHeight < getPointHeight(i, j + 1, board);
-        }
-
-        private int getPointHeight(int i, int j, int[][] board) {
-            return isPointValid(i, j, board) ? board[i][j] : Integer.MAX_VALUE;
-        }
-
-        private boolean isPointValid(int i, int j, int[][] board) {
-            return i >= 0 && i < board.length && j >= 0 && j < board[0].length;
-        }
-
-        public Integer getTotalRisk() {
-            return lowestPoints.stream().mapToInt(point -> board[point.i][point.j]).sum();
-        }
-    }
-
-    private static class BasinManager {
-        private final Set<Basin> basins = new HashSet<>();
-        private final Basin[][] pointsToBasin;
-        private final int[][] board;
-
-        public BasinManager(int[][] board) {
-            this.board = board;
-            pointsToBasin = new Basin[board.length][board[0].length];
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
-                    if (board[i][j] == 9) {
-                        continue;
-                    }
-                    Basin surroundingBasin = getSurroundingBasin(i, j);
-                    if (surroundingBasin == null) {
-                        createBasin(i, j);
-                    } else {
-                        addPointToBasin(i, j, surroundingBasin);
-                        mergeSurroundingBasins(i, j, surroundingBasin);
-                    }
-                }
+            if (!openingBraces.isEmpty()) {
+                List<String> completedClosingBraces = openingBraces.stream()
+                        .map(MATCHING_BRACES::get)
+                        .collect(Collectors.toList());
+                Collections.reverse(completedClosingBraces);
+                completedLines.add(completedClosingBraces);
             }
         }
 
-        private void addPointToBasin(int i, int j, Basin basin) {
-            basin.getPoints().add(new Point(i, j));
-            pointsToBasin[i][j] = basin;
+        public int getErrorScore() {
+            return foundErrorBraces.stream().mapToInt(ERROR_SCORES::get).sum();
         }
 
-        private Basin getSurroundingBasin(int i, int j) {
-            Basin topBasin = extractBasin(i - 1, j);
-            if (topBasin != null) {
-                return topBasin;
-            }
-            Basin bottomBasin = extractBasin(i + 1, j);
-            if (bottomBasin != null) {
-                return bottomBasin;
-            }
-            Basin leftBasin = extractBasin(i, j - 1);
-            if (leftBasin != null) {
-                return leftBasin;
-            }
-            return extractBasin(i, j + 1);
+        public long getCompletionScore() {
+            long[] scores = completedLines.stream()
+                    .mapToLong(braces -> {
+                        long score = 0;
+                        for (String brace : braces) {
+                            score = score * 5 + COMPLETION_SCORES.get(brace);
+                        }
+                        return score;
+                    })
+                    .sorted()
+                    .toArray();
+            return scores[scores.length / 2];
         }
 
-        private void createBasin(int i, int j) {
-            HashSet<Point> points = new HashSet<>();
-            points.add(new Point(i, j));
-            Basin basin = new Basin(points);
-            basins.add(basin);
-            pointsToBasin[i][j] = basin;
-        }
-
-        private void mergeSurroundingBasins(int i, int j, Basin surroundingBasin) {
-            Basin topBasin = extractBasin(i - 1, j);
-            if (topBasin != null && !surroundingBasin.equals(topBasin)) {
-                mergeBasins(surroundingBasin, topBasin);
-            }
-            Basin bottomBasin = extractBasin(i + 1, j);
-            if (bottomBasin != null && !surroundingBasin.equals(topBasin)) {
-                mergeBasins(surroundingBasin, bottomBasin);
-            }
-            Basin leftBasin = extractBasin(i, j - 1);
-            if (leftBasin != null && !surroundingBasin.equals(leftBasin)) {
-                mergeBasins(surroundingBasin, leftBasin);
-            }
-            Basin rightBasin = extractBasin(i, j + 1);
-            if (rightBasin != null && !surroundingBasin.equals(rightBasin)) {
-                mergeBasins(surroundingBasin, rightBasin);
-            }
-        }
-
-        private void mergeBasins(Basin firstBasin, Basin secondBasin) {
-            Basin biggestBasin = firstBasin.getPoints().size() > secondBasin.getPoints().size() ? firstBasin : secondBasin;
-            Basin lowestBasin = biggestBasin.equals(firstBasin) ? secondBasin : firstBasin;
-            basins.remove(lowestBasin);
-            for (Point point : lowestBasin.getPoints()) {
-                biggestBasin.getPoints().add(point);
-                pointsToBasin[point.i][point.j] = biggestBasin;
-            }
-        }
-
-        public int getTopLargestBasins(int amount) {
-            return basins.stream()
-                    .map(Basin::getPoints)
-                    .map(Collection::size)
-                    .sorted(Collections.reverseOrder())
-                    .limit(amount)
-                    .reduce(1, (a, b) -> a * b)
-                    ;
-        }
-
-        private Basin extractBasin(int i, int j) {
-            return isValid(i, j) ? pointsToBasin[i][j] : null;
-        }
-
-        private boolean isValid(int i, int j) {
-            return i >= 0 && i < board.length && j >= 0 && j < board[0].length;
-        }
-    }
-
-    @Value
-    private static class Basin {
-        UUID id = UUID.randomUUID();
-
-        Set<Point> points;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Basin basin = (Basin) o;
-            return Objects.equals(id, basin.id);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(id);
-        }
-    }
-
-    @Value
-    private static class Point {
-        int i;
-        int j;
     }
 }
