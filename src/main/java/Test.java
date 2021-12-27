0358eed8c0327d2cc6a4430c25fc71e06c61b154
vzikratyi <vzikratyi@playtika.com>
2021-12-27 20:24:25 +0200

    Day 9

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -10,7 +10,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class Test {
@@ -33,9 +35,9 @@ public class Test {
                 board[i] = boardLine;
             }
 
-            BoardRiskCalculator riskCalculator = new BoardRiskCalculator(board);
+            BasinManager basinManager = new BasinManager(board);
 
-            System.out.println(riskCalculator.getTotalRisk());
+            System.out.println(basinManager.getTopLargestBasins(3));
         }
     }
 
@@ -75,43 +77,86 @@ public class Test {
         }
     }
 
-    private static class HigherPoints {
-        private final int value;
-        private final Set<Point> points = new HashSet<>();
-        private boolean isPointLowest = false;
-
-        public HigherPoints(int value) {
-            this.value = value;
-        }
-
-        public void addPoint(int i, int j) {
-            points.add(new Point(i, j));
-        }
-
-        public void setPointLowest() {
-            isPointLowest = true;
-        }
-
-        public boolean isPointLowest() {
-            return isPointLowest;
-        }
-
-        public Set<Point> getPoints() {
-            return points;
-        }
-
-    }
-
     private static class BasinManager {
         private final Set<Basin> basins = new HashSet<>();
         private final Basin[][] pointsToBasin;
+        private final int[][] board;
 
         public BasinManager(int[][] board) {
+            this.board = board;
             pointsToBasin = new Basin[board.length][board[0].length];
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (board[i][j] == 9) {
+                        continue;
+                    }
+                    Basin surroundingBasin = getSurroundingBasin(i, j);
+                    if (surroundingBasin == null) {
+                        createBasin(i, j);
+                    } else {
+                        addPointToBasin(i, j, surroundingBasin);
+                        mergeSurroundingBasins(i, j, surroundingBasin);
+                    }
+                }
+            }
         }
 
-        public void connectPoints(Point first) {
+        private void addPointToBasin(int i, int j, Basin basin) {
+            basin.getPoints().add(new Point(i, j));
+            pointsToBasin[i][j] = basin;
+        }
 
+        private Basin getSurroundingBasin(int i, int j) {
+            Basin topBasin = extractBasin(i - 1, j);
+            if (topBasin != null) {
+                return topBasin;
+            }
+            Basin bottomBasin = extractBasin(i + 1, j);
+            if (bottomBasin != null) {
+                return bottomBasin;
+            }
+            Basin leftBasin = extractBasin(i, j - 1);
+            if (leftBasin != null) {
+                return leftBasin;
+            }
+            return extractBasin(i, j + 1);
+        }
+
+        private void createBasin(int i, int j) {
+            HashSet<Point> points = new HashSet<>();
+            points.add(new Point(i, j));
+            Basin basin = new Basin(points);
+            basins.add(basin);
+            pointsToBasin[i][j] = basin;
+        }
+
+        private void mergeSurroundingBasins(int i, int j, Basin surroundingBasin) {
+            Basin topBasin = extractBasin(i - 1, j);
+            if (topBasin != null && !surroundingBasin.equals(topBasin)) {
+                mergeBasins(surroundingBasin, topBasin);
+            }
+            Basin bottomBasin = extractBasin(i + 1, j);
+            if (bottomBasin != null && !surroundingBasin.equals(topBasin)) {
+                mergeBasins(surroundingBasin, bottomBasin);
+            }
+            Basin leftBasin = extractBasin(i, j - 1);
+            if (leftBasin != null && !surroundingBasin.equals(leftBasin)) {
+                mergeBasins(surroundingBasin, leftBasin);
+            }
+            Basin rightBasin = extractBasin(i, j + 1);
+            if (rightBasin != null && !surroundingBasin.equals(rightBasin)) {
+                mergeBasins(surroundingBasin, rightBasin);
+            }
+        }
+
+        private void mergeBasins(Basin firstBasin, Basin secondBasin) {
+            Basin biggestBasin = firstBasin.getPoints().size() > secondBasin.getPoints().size() ? firstBasin : secondBasin;
+            Basin lowestBasin = biggestBasin.equals(firstBasin) ? secondBasin : firstBasin;
+            basins.remove(lowestBasin);
+            for (Point point : lowestBasin.getPoints()) {
+                biggestBasin.getPoints().add(point);
+                pointsToBasin[point.i][point.j] = biggestBasin;
+            }
         }
 
         public int getTopLargestBasins(int amount) {
@@ -120,15 +165,37 @@ public class Test {
                     .map(Collection::size)
                     .sorted(Collections.reverseOrder())
                     .limit(amount)
-                    .mapToInt(i -> i)
-                    .sum()
+                    .reduce(1, (a, b) -> a * b)
                     ;
+        }
+
+        private Basin extractBasin(int i, int j) {
+            return isValid(i, j) ? pointsToBasin[i][j] : null;
+        }
+
+        private boolean isValid(int i, int j) {
+            return i >= 0 && i < board.length && j >= 0 && j < board[0].length;
         }
     }
 
     @Value
     private static class Basin {
+        UUID id = UUID.randomUUID();
+
         Set<Point> points;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Basin basin = (Basin) o;
+            return Objects.equals(id, basin.id);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
     }
 
     @Value
