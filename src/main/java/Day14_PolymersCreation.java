04266a45b84e41f56dfff5714b1db06d4e0e15c2
vzikratyi <vzikratyi@playtika.com>
2022-01-01 21:21:13 +0200

    Day 14 (Polymers creation)

--- src/main/java/Day14_PolymersCreation.java
+++ src/main/java/Day14_PolymersCreation.java
@@ -7,12 +7,11 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day14_PolymersCreation {
     public static void main(String[] args) throws Exception {
@@ -23,65 +22,98 @@ public class Day14_PolymersCreation {
             List<String> lines = br.lines().collect(Collectors.toList());
             String initialTemplate = lines.get(0);
 
-            Polymer polymer = new Polymer(initialTemplate, lines.subList(1, lines.size()));
+            Polymer polymer = new Polymer(initialTemplate, lines.subList(1, lines.size()), 40);
 
-            polymer.simulateSteps(20);
-
-            System.out.println(polymer.getSize());
             System.out.println(polymer.getResult());
         }
     }
 
     private static class Polymer {
-        private final Map<List<String>, String> pairInsertionRules;
-        private final LinkedList<String> polymer;
+        private final Map<Integer, Map<List<String>, DistinctElementsCounter>> elementsCountDictionary = new HashMap<>();
 
-        public Polymer(String initialPolymer, Collection<String> pairInsertionRulesCollection) {
-            this.polymer = Arrays.stream(initialPolymer.split("")).collect(Collectors.toCollection(LinkedList::new));
-            this.pairInsertionRules = pairInsertionRulesCollection.stream()
+        private final Map<List<String>, String> initialPairInsertionRules;
+        private final List<String> initialPolymer;
+        private final int maxSteps;
+
+        public Polymer(String initialPolymerStr, Collection<String> pairInsertionRulesCollection, int maxSteps) {
+            this.initialPolymer = Arrays.stream(initialPolymerStr.split("")).collect(Collectors.toCollection(LinkedList::new));
+            this.initialPairInsertionRules = pairInsertionRulesCollection.stream()
                     .filter(Predicate.not(String::isBlank))
                     .map(str -> str.split(" -> "))
                     .collect(Collectors.toMap(
                             elements -> Arrays.stream(elements[0].split("")).collect(Collectors.toList()),
                             elements -> elements[1]));
-        }
-
-        public void simulateSteps(int steps) {
-            for (int i = 0; i < steps; i++) {
-                simulateStep();
-            }
-        }
-
-        private void simulateStep() {
-            ListIterator<String> iterator = polymer.listIterator();
-            while (true) {
-                String element = iterator.next();
-                if (!iterator.hasNext()) {
-                    return;
-                }
-                String nextElement = iterator.next();
-                iterator.previous();
-                String createdElement = pairInsertionRules.get(List.of(element, nextElement));
-                if (createdElement != null) {
-                    iterator.add(createdElement);
-                }
-            }
+            this.maxSteps = maxSteps;
         }
 
         public long getResult() {
-            Map<String, AtomicLong> elementQuantities = new HashMap<>();
-            for (String element : polymer) {
-                elementQuantities.computeIfAbsent(element, ignored -> new AtomicLong()).incrementAndGet();
+            DistinctElementsCounter elementsCounter = DistinctElementsCounter.of(initialPolymer);
+            for (int i = 1; i < initialPolymer.size(); i++) {
+                DistinctElementsCounter foundElementCounter = getElementsCounter(0, initialPolymer.get(i - 1), initialPolymer.get(i));
+                elementsCounter = elementsCounter.merge(foundElementCounter);
             }
-            long[] quantities = elementQuantities.values().stream()
-                    .mapToLong(AtomicLong::get)
+
+            long[] quantities = elementsCounter.elementsCount.values().stream()
+                    .mapToLong(l -> l)
                     .sorted()
                     .toArray();
             return quantities[quantities.length - 1] - quantities[0];
         }
 
-        public int getSize() {
-            return polymer.size();
+        private DistinctElementsCounter getElementsCounter(int step, String left, String right) {
+            String generatedElement = initialPairInsertionRules.get(List.of(left, right));
+            if (generatedElement == null) {
+                return DistinctElementsCounter.EMPTY;
+            }
+            if (step == maxSteps - 1) {
+                return DistinctElementsCounter.of(generatedElement);
+            }
+            Map<List<String>, DistinctElementsCounter> stepDictionary = elementsCountDictionary.computeIfAbsent(step, ignored -> new HashMap<>());
+            DistinctElementsCounter leftPair = stepDictionary.computeIfAbsent(List.of(left, generatedElement), ignored ->
+                    getElementsCounter(step + 1, left, generatedElement));
+            DistinctElementsCounter rightPair = stepDictionary.computeIfAbsent(List.of(generatedElement, right), ignored ->
+                    getElementsCounter(step + 1, generatedElement, right));
+
+            return leftPair.merge(rightPair).addElement(generatedElement);
+        }
+    }
+
+    private static class DistinctElementsCounter {
+        public static final DistinctElementsCounter EMPTY = new DistinctElementsCounter(Map.of());
+
+        public static DistinctElementsCounter of(String element) {
+            return new DistinctElementsCounter(Map.of(element, 1L));
+        }
+
+        public static DistinctElementsCounter of(List<String> elements) {
+            Map<String, Long> elementsCount = new HashMap<>();
+            for (String element : elements) {
+                elementsCount.merge(element, 1L, Long::sum);
+            }
+            return new DistinctElementsCounter(elementsCount);
+        }
+
+        private final Map<String, Long> elementsCount;
+
+        private DistinctElementsCounter(Map<String, Long> elementsCount) {
+            this.elementsCount = elementsCount;
+        }
+
+        public DistinctElementsCounter merge(DistinctElementsCounter other) {
+            Map<String, Long> newElementsCount = Stream.of(this.elementsCount, other.elementsCount)
+                    .flatMap(map -> map.entrySet().stream())
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            Long::sum));
+            return new DistinctElementsCounter(newElementsCount);
+        }
+
+
+        public DistinctElementsCounter addElement(String element) {
+            HashMap<String, Long> newElementsCount = new HashMap<>(elementsCount);
+            newElementsCount.merge(element, 1L, Long::sum);
+            return new DistinctElementsCounter(newElementsCount);
         }
     }
 }
